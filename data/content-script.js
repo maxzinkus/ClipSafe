2054c22a71da62d00cd9299aa5d9d2d3717afa6a
Max Zinkus <mzinkus@calpoly.edu>
2016-11-04 00:51:09 -0700

    Condense code down

--- data/content-script.js
+++ data/content-script.js
@@ -1,13 +1,11 @@
 
-function clipsafe() {
-    self.port.emit("clipsafe trigger", "clipsafe trigger");
-}
-
 function sweep() {
-    var hcol = document.getElementsByTagName('input');
+    var hcol = document.getElementsByTagName("input");
     for (x = 0; x < hcol.length; x++) {
         if (hcol[x].onpaste === null && hcol[x].type === "password") {
-            hcol[x].addEventListener("paste", clipsafe);
+            hcol[x].addEventListener("paste", function() {
+                self.port.emit("clipsafe trigger", "clipsafe trigger");
+            });
         }
     }
 }
--- index.js
+++ index.js
@@ -5,14 +5,12 @@ let { Cc, Ci } = require("chrome");
 const gClipboardHelper = Cc['@mozilla.org/widget/clipboardhelper;1']
                             .getService(Ci.nsIClipboardHelper);
 
-function clipsafe() {
-    gClipboardHelper.copyString("");
-}
-
 function runClipSafe(tab) {
     var worker = tab.attach({
         contentScriptFile: self.data.url("content-script.js")
     });
-    worker.port.on("clipsafe trigger", function(payload) { clipsafe(); });
+    worker.port.on("clipsafe trigger", function(payload) {
+            gClipboardHelper.copyString("");
+    });
 }
 
